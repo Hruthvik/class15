e70508d5ed0a08945e55e1a11f42a60aeb13c054
Hruthvik <34919558+Hruthvik@users.noreply.github.com>
2022-06-14 00:05:24 +0900

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM ubuntu:20.04
+
+WORKDIR /root
+COPY ./Dockerfile /root
+
+RUN apt-get update
+RUN apt-get install -y git
+RUN apt-get install -y python
+RUN apt-get install -y python3-pip
+RUN pip3 install uvicorn
+RUN pip3 install fastapi
+RUN pip3 install httptools==0.1.
+
+RUN mkdir /root/assignment
+WORKDIR /root/assignment
+RUN git clone https://github.com/Hruthvik/class15.git
+WORKDIR /root/assignment/class15
+
+
+CMD ["uvicorn","compute:app1","--reload","--host","0.0.0.0","--port","8080"]
+
+
---
+++ compute.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+import statistics
+
+app1 = FastAPI()
+
+numbers = []
+
+@app1.get("/")
+async def showMessage():
+    return {"response": "this is the root. Nothing else."}
+
+@app1.post("/numbers")
+async def getnumber(new):
+    if new.isdigit() == True:
+        numbers.append(int(new))
+        return {"result": "OK"}
+    else:
+        return {"result": "Error"}
+
+@app1.get("/numbers/average")
+async def average():
+    avg = 0
+    if len(numbers) > 0:
+        for i in numbers:
+            avg = avg + i
+        avg = avg/(len(numbers))
+        return {"result ": avg}
+    else:
+        return {"No numbers in the array"}
+
+@app1.get("/numbers/sum")
+async def sum():
+    s = 0
+    if len(numbers) > 0:
+        for i in numbers:
+            s = s + i
+        return {"result ": s}
+    else:
+        return {"No numbers in the array"}
+        
+ @app.get("/numbers/stddev")
+async def getStdev():
+    if (numbers == []):
+        return {"result": "No numbers in the array"}
+    else:
+        stddev = statistics.stdev(numbers)
+        return {"result": stddev }      
+
